e2c7be775ea1e1e22e7c4f0d5dc1eae9c4751d38
Aran Reeks <cdtreeks@gmail.com>
2018-12-02 08:35:17 +0000

    PHP solution to Day02

---
+++ day02/day02.php
@@ -0,0 +1,60 @@
+<?php
+$input = 'qywzphxoiseldjrbaoagvkmanu,qywzphxeisulpjrbtfcgvkmanu,qywzxhooiseldjrbtfcgvcmanu,qywzphjojseldjubtfcgvkmanu,qtwzphxoieeldjrbtfcgvrmanu,tywzphzoiseldjritfcgvkmanu,qyuzphxoiseldjrbtfcgykbanu,qswzmhxoiseldjrbtfcgvkaanu,qyczqhxoiseldjrbtfcgvkbanu,qybzpqxooseldjrbtfcgvkmanu,qywzpoxoxseldjrbtfcgvpmanu,qywzphxbipeldjrbtfcgvkmaru,qywzpmxoiseldjrutqcgvkmanu,qywzphxoisesdjrrtfchvkmanu,qywzphxoiseldhrbtfcgukyanu,jywzphyoiseldjrbtfclvkmanu,qywzvhxoiselrjrbtfcgvkmanl,qywzplxojseldjrbtfcgvkmamu,qywzphxoiseldjrbtacgvkmvnd,qywpphxoiseldjrbtfcgvkvenu,qqazphxoiselqjrbtfcgvkmanu,qyozvhxoiseldjrbtfcgvkmadu,xywzphxliseldjhbtfcgvkmanu,qdwzphioiseldqrbtfcgvkmanu,qswyphxoiseldjrbtfcgvkmanx,qowzshxoiseldjrbtfigvkmanu,qywzphxoiseldjjbtfcgvkmawq,qyhzphxoiseldjrbtfvgvkmynu,qxwzphxoiselmjrbtfcgvqmanu,qywzphxoiceldjrbtfcgikmgnu,qywephxoiseldjrbthcgvkmane,yywzphxoisejdjrotfcgvkmanu,qywzxhxoisyldyrbtfcgvkmanu,qywzpjxoiseqdjrbtfcgvwmanu,qywzphxoqweldprbtfcgvkmanu,qywzphnoiseldjrbtfcamkmanu,qylgphxoiseldjrbtfcgvkmauu,qysdphxomseldjrbtfcgvkmanu,lywzpoxoikeldjrbtfcgvkmanu,qywzphxgiseldjrbtfeqvkmanu,qywzdhxozseldjcbtfcgvkmanu,qywzphxeiskedjrbtfcgvkmanu,qmwzphxoispldjrbtfcgvkmany,qywzphxoiselzcrbtfcgvkmanz,qywzphxoisxodjrbtscgvkmanu,qywzphxoiseldjrrtfcgmkmaeu,aywzphxoiseldjrbtfmjvkmanu,qywzthxoiiewdjrbtfcgvkmanu,qywzpsxoisrzdjrbtfcgvkmanu,qywzphxoiseldjkbtqcgvkmank,tywzphxoisewdjkbtfcgvkmanu,qywkchxoiseldjrbtfcghkmanu,qywzphxoiseldjoftfrgvkmanu,qywzpixoisehdjrbtfcgvkmacu,qywzchooiseldarbtfcgvkmanu,qywyphtoaseldjrbtfcgvkmanu,qywyphxotsbldjrbtfcgvkmanu,qywzphxoiseldjrbhsggvkmanu,dywzphxoiseldjrktfegvkmanu,qywzphxoiseldjrmtfcgvkcenu,qywcphxjiseldjybtfcgvkmanu,qywzphxoiseldhrbsfogvkmanu,qmwzpheoiseldjrrtfcgvkmanu,qywzrhxoiselpjrbtfcgvemanu,qyezphpoiseldjrbtfcgvdmanu,qywzphxvisewdjrbtfcgvkmdnu,qywzphkoiseldjrbtfcgvkmdnh,qywzehxoiseldfrbtpcgvkmanu,qywzphxoiseldjrrtfcgvrdanu,qpwzphxoizeldjqbtfcgvkmanu,qywzphxojseldjrbtmcgvkmvnu,vywzphxoiseldjrbtfcgvkmaop,sywzphxoiselbjrttfcgvkmanu,qywwphkoiseldjrbtfcivkmanu,qlwkpcxoiseldjrbtfcgvkmanu,qywzphxoiyesdjrbtfcgvkmvnu,qywzphxoiseldjrbofcgrkmrnu,qywzphxyiseldjrstfcgvkmjnu,qywzphaoiseldtrbnfcgvkmanu,qywzphxhisuldurbtfcgvkmanu,qywzphxdiseldjrbtvugvkmanu,qywzpzxbiseldjrbtfcgukmanu,qyrzphxoixeldjrbtfcgvumanu,qywzphxoiberdjrptfcgvkmanu,qywzphxfiskldjrbtfcgvkmdnu,qyxzphxoiseldjrdhfcgvkmanu,qywzphrqiseldjrbtfcgvbmanu,qyezphxviseldjrbtfcgvkmani,qywgphxoiseldgkbtfcgvkmanu,qywzphxoileldjrbtgcgvkdanu,qywzphxoiseldnrbtfcwvkmpnu,qywzphxoiseidjrbmfcqvkmanu,qywzpkxoiselijrbtfcgvlmanu,mywzphxoiveldjrbtfcgvkmunu,qywzphxooseddjrbtfcgpkmanu,qywzphxokseldjritfcyvkmanu,qywzxhxoiseldjrbtfqgvcmanu,qywzphxoisfldjrbpfcgvkmaju,qywuphxgiseldjrbffcgvkmanu,qywzphociseldjrbtfcgvkuanu,qywzphxoiseldvrbtftgckmanu,qywzpbxoisrldjrbtfngvkmanu,qywzphxoiseldjrntfygvdmanu,qywzphxviseldkrbtfcgvkianu,qywpphxgiseldjrbtfctvkmanu,qywzphxoicewdjrbtfcgvsmanu,qywzpcxoiseldjmbtfcgvcmanu,qrwzphxoiseldjrbtfcgjumanu,qywzphxoiselojrbtfcgxkmaau,qywzphxojsbldjrbtfcgykmanu,oywzphxoiseldjrbtfqgvkmvnu,qywfphxpiseldjrbtfckvkmanu,qyyzwhxwiseldjrbtfcgvkmanu,qywzphxgiseldjrbtfchvkmabu,qywzphxfiseldjrbtfcgukoanu,qywzpdxoisyldjrbtfcgvkxanu,dyuzphxoiseldjrbtfcgvkmamu,qywzphxoiseldjrbifcgvkmnnp,qywzpyxoiseldjrbtfcgvklano,dywzphxoiieldjrbtfcgvwmanu,qywzphxoihemdjrbtfcgvdmanu,gywzphxoxseldvrbtfcgvkmanu,qywzqhxoissldjwbtfcgvkmanu,eywzphxoiieldjrbtfcgekmanu,qyizprxciseldjrbtfcgvkmanu,qywjphxoiseldjrbtfcgckmano,qywznhxoiseldjrbrfcgvkmagu,qywzphxoisrldjdbvfcgvkmanu,qyyxphxoiseldjrbtwcgvkmanu,qywzphxoiseldjdbtfcpvkmjnu,qywzvhxqiseldjrbofcgvkmanu,vywzphxoiseldjrbtfcgckwanu,qywzphgbiseldjrbtfcgvkmazu,qcwzphxoiseldjrbqfcgvkmdnu,qywzphxoismldjrbtfcgkkmznu,qywhphxoiseldjrbtccgvkmane,qywzphzoiseldjrbtfcgvqmauu,hywzphxoiseldjrbtfcuvkmanc,qywzphxozsejdgrbtfcgvkmanu,qyszphxoiseldjrntfygvdmanu,qywzphxoisgldjrbtfcgvklaru,qywzhhxoiseldjrbtscgvkmqnu,qywjphxpiqeldjrbtfcgvkmanu,qywzphxoiseldxrptfclvkmanu,qywlphxoisehdjrbtfcgvkmanc,qydzpfxoiseldjrwtfcgvkmanu,qywzphxoiseldjrbtxcgqkfanu,qywophxoiselfjrbtfcgvkmani,qywzyhxoiszldtrbtfcgvkmanu,qywzphxoxseldfrntfcgvkmanu,qywzphloiseldjqbtfcgvkmtnu,qywzpuxoiseldorbtfcgvkeanu,qywzphxoiueldjrwdfcgvkmanu,qgwzphxoiseldjmbtncgvkmanu,qywzphtggseldjrbtfcgvkmanu,qywzphxoisrldjrbtfmgvhmanu,qfszphxoiseldjqbtfcgvkmanu,qywzphxpisjldjrbxfcgvkmanu,qywznhxoisepdjrbtfqgvkmanu,qywzphioiseldjabtfcgxkmanu,qyizphxaiseldjrbtfcgvkmaxu,xywzphxoiqelvjrbtfcgvkmanu,quwzphxoiseldjretfcgvkmaau,bywzphxoiseldjrbtucgckmanu,jywzphxoiseldjrbofcgvkmani,qywzphxoiseltjkbtfcgvkmabu,eywzphxoiselgjrbtfkgvkmanu,qywzphxoisengjrttfcgvkmanu,qywzphzoiseldjrbtfcgvkmknk,quwdphxoiseldjrbtfcxvkmanu,qcwzzhxoiseldjrgtfcgvkmanu,qywgphxdiseldjrbtfcgjkmanu,qywzpdxoivefdjrbtfcgvkmanu,qywzphxoiseldjrbtfdgvjmpnu,qfwzphxoiseydkrbtfcgvkmanu,qywzphxdiqelqjrbtfcgvkmanu,qywzvhxoiseldjrbtfognkmanu,qywzphgoiseldjrbcfcgvtmanu,qywophxoiseldjrbtpcgvkmank,qywzphxoiszldjretfcgvkmabu,qywzphxoiseldjhbtfcgvxmawu,qcgzphxoiselejrbtfcgvkmanu,qywzphxoisepdjrbtfcfvkcanu,qivzphxniseldjrbtfcgvkmanu,qywzhhxoiseldjrftxcgvkmanu,qyazphxciseldjrbtfcgskmanu,qywzphxoisoldgrbtfczvkmanu,qywzmhxoiseldurbwfcgvkmanu,qywzphxoistldjrbwfcgvkranu,qywzphxoistedjrbtfcgokmanu,qywzqhxodsecdjrbtfcgvkmanu,qywzphxtisxldjrbtfcgvkhanu,qywzphxoeseldjrtrfcgvkmanu,qdwzphxoioeldjrbtfigvkmanu,qjwzphxoisbldjrbtfcgvkmanz,qywzphxoiseldbrbtfdgvlmanu,qywzphxoiselddrbhvcgvkmanu,zywzppxoiseldjrdtfcgvkmanu,qywzppxqiselkjrbtfcgvkmanu,qywzphxoihelbjrbtfcgvkmabu,qywzphxoiseldjreyfcgvknanu,qywzphxxrseldjrbtfcgvkmagu,qywhpfxoiseldjrbtfdgvkmanu,qywzphxoisxldjrdtfagvkmanu,oywzphxxiseldjrbtfcgvkmaeu,qywzphxoiselqirbtfvgvkmanu,qywzphxoqseldhrbtfcgvkhanu,qywzphxokseldjrbtfxgvkmaju,qywzphtoiseldurbtfcfvkmanu,qywzphxoiheudjrrtfcgvkmanu,qrwzphxzigeldjrbtfcgvkmanu,qywzphxoiseldorbtfcgvxmvnu,qywzphxoisaldjpbqfcgvkmanu,qywuphxoiselljrbtfchvkmanu,qywzphxoisaldjrbefcgvkmsnu,qywzphxoiteldjrbnfcgvkmanp,qywhphxoiselqjrbtfcgvkmagu,qywzjhxoisejdjrbtfcgvkmanr,qywephaoiseldjrbtfcavkmanu,qywcphxoireldjqbtfcgvkmanu,qywzphxoiseldirbuicgvkmanu,qywzphxoisecvnrbtfcgvkmanu,qcwzphxoiseldjrbtfcgvrmaiu,qywnphxoiseldjrntftgvkmanu,qywzphxhisyldjrbtfcgvkmafu,qyhzphxoiseldjrytfcgvkmanq,vjwzbhxoiseldjrbtfcgvkmanu,qyvzpsxoisuldjrbtfcgvkmanu,qywzphxaiseldcrbefcgvkmanu,qywzphxoiseldjgbtfsgvkfanu,oiwzphxoiseldjrbtfcgvkmcnu,qyezphxoiseldjrbtfcqvkmjnu,tywzphxriseldzrbtfcgvkmanu,yywzphxoiseldjbbtfugvkmanu,qywzpfxviseldjrbttcgvkmanu,qywzphxoiceldcrbtfugvkmanu,qymzphxoiseldjratfcsvkmanu,qywzphxoiselxjrbdfcgvkpanu,qywzphxoiselujrbtfkgvimanu,qywzshyoiseldjrbtfcgpkmanu,qywzphxoiselfjrbtfsgvkmant,qywpphxoiseldjxbtfcyvkmanu,qywzfhxoiselqjrptfcgvkmanu,qewzphxoiseldprbtfcgvkmand,qywfphxoiseldlrbtfcgvkmgnu,qywzphxoiseldjhbtqcovkmanu,fywzphxoiseldlrbtfcgvkjanu,sywzphxoiseldjrbhfccvkmanu,qywzphxoiseldjfbtfcrvkmpnu,sywzphxoisrldjrbtfczvkmanu';
+
+$matches = [
+	2 => 0,
+	3 => 0,
+];
+$similar_inputs = [];
+
+$inputs = explode(',', $input);
+
+// Part 1 = 5478
+foreach ($inputs as $input) {
+	$len = strlen($input);
+	$current_count = [];
+	for ($i = 0; $i < $len; $i++) {
+		if (isset($current_count[$input[$i]])) {
+			$current_count[$input[$i]]++;
+		} else {
+			$current_count[$input[$i]] = 1;
+		}
+	}
+
+	$found_3 = $found_2 = false;
+	foreach ($current_count as $count) {
+		if ($count === 3 && $found_3 === false) {
+			$found_3 = true;
+			$matches[3]++;
+			$similar_inputs[] = $input;
+		} else if ($count === 2 && $found_2 === false) {
+			$found_2 = true;
+			$matches[2]++;
+			$similar_inputs[] = $input;
+		}
+	}
+}
+
+echo 'Part 1: ' . $matches[2] * $matches[3] . PHP_EOL;
+
+// Part 2
+$best_answer = '';
+foreach ($similar_inputs as $i => $input) {
+	$len = strlen($input);
+	foreach ($similar_inputs as $ii => $sub_input) {
+		if ($i === $ii) continue;
+
+		$matching = '';
+		for ($iii = 0; $iii < $len; $iii++) {
+			if ($input[$iii] === $sub_input[$iii]) {
+				$matching .= $input[$iii];
+			}
+		}
+
+		if ($matching && strlen($matching) === $len-1) {
+			$best_answer = $matching;
+		}
+	}
+}
+
+echo 'Part 2: ' . $best_answer . PHP_EOL;
